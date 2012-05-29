ddf2b459ec64462126b31ea3469501aaa0e7ad10
Sandor Borde <sborde@sborde-G33M-S2L.(none)>
2012-05-29 16:26:01 +0200

    Revert "First commit"
    This reverts commit ffd662df60bb62ede5f748a0e3aac010907cafce.

--- src/gossipLearning/models/bandits/BanditModel.java
+++
@@ -1,9 +0,0 @@
-package gossipLearning.models.bandits;
-
-import gossipLearning.interfaces.Model;
-
-public interface BanditModel extends Model {
-  public double predict(int armIdx);
-  public double numberOfPlayes(int armIdx);
-  public double numberOfAllPlayes();
-}
